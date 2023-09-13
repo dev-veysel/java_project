845bfdbc8aabfd36d186fbe92435130ad5fc1158
Veysel <veysel-celik@gmx.de>
2023-09-13 14:53:58 +0200

    Initial Commit
    Added java project

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+H
+src/main/java/hello.java,a\7\a76a77191a988a3a4c8fa7df8d8c187cb70b3908
---
+++ src/main/java/hello.java
@@ -0,0 +1,14 @@
+public class hello{
+
+
+public static void main(String[] args) {
+    
+for(int i=1; i<10; i++){
+    System.out.println("Ich kann bis 10 Zählen, also... "+i);
+    if(i==9){
+        System.out.println("Uuuuuuuund "+(i+1));
+    }
+}
+
+}
+}
